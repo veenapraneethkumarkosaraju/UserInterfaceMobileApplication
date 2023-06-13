1293918a38cbeeefc6f473ee090d3afc225e9a24
veenapraneeth123@gmail.com <veenapraneeth123@gmail.com>
2023-06-13 15:08:49 -0400

    veena

---
+++ README.md
@@ -0,0 +1,3 @@
+# SocialMedia-Demo
+This is a sample social media application
+![image](https://user-images.githubusercontent.com/36036731/235038126-7fe1cf8e-1542-4930-9543-b513032f2461.png)
---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express=require('express');
+const app=express();
+const mongoose = require("mongoose");
+
+const path=require('path');
+
+const userRoutes = require('./server/routes/user');
+const postRoutes= require('./server/routes/post');
+
+mongoose.connect(process.env.dbURL)
+  .then(console.log("DB Connected!!"))
+  .catch(error => console.log(error));
+app.use(express.json());
+
+app.use(express.static(__dirname + "/public"))
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'/public','index.html')));
+
+
+
+
+app.use(function(req,res,next){
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin,  X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
+    next();
+});
+
+
+
+app.use('/user',userRoutes);
+app.use('/post',postRoutes);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
